ad3fe7a54c517845d32b25831556f8a4de763faf
krish-mamtora <krish.mamtora@roimaint.com>
2026-02-20 15:10:15 +0530

    Navbar theme added using Zustand store

---
+++ src/store/useThemeStore.ts
@@ -0,0 +1,18 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+interface ThemeState {
+  isDarkMode: boolean;
+  toggleTheme: () => void;
+}
+
+const useThemeStore = create<ThemeState>()(
+  persist(
+    (set) => ({
+      isDarkMode: false,
+      toggleTheme: () => set((state) => ({ isDarkMode: !state.isDarkMode })),
+    }), {
+    name: "theme-value",
+  }
+  )
+);
+export default useThemeStore;
